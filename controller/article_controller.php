2acb64c6902e65bccdee0ed8a112303011344939
t.suzuki <tkft.psy@gmail.com>
2020-06-26 22:14:11 +0900

    db

--- controller/article_controller.php
+++ controller/article_controller.php
@@ -3,7 +3,7 @@
   header("Content-type: text/html; charset=utf-8");
   // ログイン確認
   if(!isset($_SESSION["user_id"])) {
-    $no_login_url = "index.php";
+    $no_login_url = "../index.php";
     header("Location: {$no_login_url}");
     exit;
   }
--- controller/user_controller.php
+++ controller/user_controller.php
@@ -2,7 +2,7 @@
   session_start();
   header("Content-type: text/html; charset=utf-8");
   if(!isset($_SESSION["user_id"])) {
-    $no_login_url = "index.php";
+    $no_login_url = "../index.php";
     header("Location: {$no_login_url}");
     exit;
   }
